3f02062f2791f372c83b6364ba5fa15f5bb8e022
Guilherme-silva0 <guilhermesilvadev0@gmail.com>
2023-06-20 22:56:56 -0300

    :sparkles: feat: Creating Header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,42 @@
+'use client'
+
+import { useDrawerContext } from '@/contexts/DrawerContext'
+import {
+  IconButton,
+  Stack,
+  Typography,
+  useMediaQuery,
+  useTheme,
+} from '@mui/material'
+import { MenuIcon } from 'lucide-react'
+
+interface IHeaderProps {
+  title: string
+}
+
+const Header = ({ title }: IHeaderProps) => {
+  const theme = useTheme()
+  const smDown = useMediaQuery(theme.breakpoints.down('sm'))
+  const { toggleDrawerOpen } = useDrawerContext()
+  return (
+    <Stack direction="row" alignItems="center" gap={0.5} padding={2}>
+      {smDown && (
+        <IconButton onClick={toggleDrawerOpen}>
+          <MenuIcon size={28} />
+        </IconButton>
+      )}
+      <Typography
+        variant={smDown ? 'h5' : 'h4'}
+        fontWeight="500"
+        whiteSpace="nowrap"
+        overflow="hidden"
+        textOverflow="ellipsis"
+        component="h2"
+      >
+        {title}
+      </Typography>
+    </Stack>
+  )
+}
+
+export default Header
